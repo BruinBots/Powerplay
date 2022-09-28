647cddcef3f0f3a49c8cf72cff224aaec08a6230
rocky <rockyjshaikh@gmail.com>
2022-09-28 17:26:14 -0400

    made wrapper class and op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Karen.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Karen  {
+
+
+    // Class variables
+
+    public DcMotorEx leftFrontMotor;
+    public DcMotorEx rightFrontMotor;
+    public DcMotorEx leftBackMotor;
+    public DcMotorEx rightBackMotor;
+
+    public DigitalChannel leftFrontSwitch;
+    public DigitalChannel rightFrontSwitch;
+
+    //
+
+    // constructor with map
+    public Karen (HardwareMap map) {
+        // Drivetrain Motors
+        DcMotorEx leftFrontMotor = map.get(DcMotorEx.class, "leftFrontMotor");
+        DcMotorEx rightFrontMotor = map.get(DcMotorEx.class, "rightFrontMotor");
+        DcMotorEx leftBackMotor = map.get(DcMotorEx.class, "leftBackMotor");
+        DcMotorEx rightBackMotor = map.get(DcMotorEx.class, "rightBackMotor");
+
+        leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        // Front Switches
+        leftFrontSwitch = map.get(DigitalChannel.class, "leftFrontSwitch");
+        rightFrontSwitch = map.get(DigitalChannel.class, "rightFrontSwitch");
+    }
+
+    public void moveBot(double drive, double rotate, double strafe, double scaleFactor) {
+        double[] wheelSpeeds = new double[4];
+        wheelSpeeds[0] = drive + strafe + rotate;  // left front
+        wheelSpeeds[1] = drive - strafe - rotate;  // right front
+        wheelSpeeds[2] = drive - strafe + rotate;  // left rear
+        wheelSpeeds[3] = drive + strafe - rotate;  // right rear
+
+        // finding the greatest power value
+        double maxMagnitude = Math.max(Math.max(Math.max(wheelSpeeds[0], wheelSpeeds[1]), wheelSpeeds[2]), wheelSpeeds[3]);
+
+
+        // dividing everyone by the max power value so that ratios are same (check if sdk automatically clips to see if go build documentation works
+        if (maxMagnitude > 1.0)
+        {
+            for (int i = 0; i < wheelSpeeds.length; i++)
+            {
+                wheelSpeeds[i] /= maxMagnitude;
+            }
+        }
+
+
+
+        // setting motor power and scaling down to preference
+        leftFrontMotor.setPower(wheelSpeeds[0] * scaleFactor);
+        rightFrontMotor.setPower(wheelSpeeds[1] * scaleFactor);
+        leftBackMotor.setPower(wheelSpeeds[2] * scaleFactor);
+        rightBackMotor.setPower(wheelSpeeds[3] * scaleFactor);
+
+    }
+
+    public void stop(){
+        leftFrontMotor.setPower(0);
+        leftBackMotor.setPower(0);
+        rightFrontMotor.setPower(0);
+        rightBackMotor.setPower(0);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOpMode.java
@@ -36,107 +36,54 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
 import org.firstinspires.ftc.robotcontroller.external.samples.SensorDigitalTouch;
-
-/**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 public class MecanumOpMode extends OpMode
 {
     // Declare OpMode members.
 
-
-    public DcMotor frontLeft;
-    public DcMotor frontRight;
-    public DcMotor backLeft;
-    public DcMotor backRight;
-
-
-    public DigitalChannel leftFrontSwitch;
-    public DigitalChannel rightFrontSwitch;
-
-
-
-
-
-
-
     double drive = 0.0;
     double turn = 0.0;
     double strafe = 0.0;
 
+    Karen bot;
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
+    //
     @Override
     public void init() {
 
-        frontLeft = hardwareMap.get(DcMotor.class, "leftFrontDrive");
-        frontRight = hardwareMap.get(DcMotor.class, "rightFrontDrive");
-        backLeft = hardwareMap.get(DcMotor.class, "leftRearDrive");
-        backRight = hardwareMap.get(DcMotor.class, "rightRearDrive");
-
-        leftFrontSwitch = hardwareMap.get(DigitalChannel.class, "leftFrontSwitch");
-        rightFrontSwitch = hardwareMap.get(DigitalChannel.class, "rightFrontSwitch");
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        // Tell the driver that initialization is complete.
+        bot = new Karen(hardwareMap);
         telemetry.addData("Status", "Initialized");
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
+    //
     @Override
     public void init_loop() {
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
+    //
     @Override
     public void start() {
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
+    //
     @Override
     public void loop() {
         drive = -gamepad1.left_stick_y * 0.5;
         strafe = gamepad1.left_stick_x * 0.5;
         turn = gamepad1.right_stick_x * 0.5;
 
-        frontLeft.setPower(drive + strafe + turn);
-        frontRight.setPower(drive - strafe - turn);
-        backLeft.setPower(drive - strafe + turn);
-        backRight.setPower(drive + strafe - turn);
+        bot.moveBot(drive, turn, strafe, 0.3);
 
-        telemetry.addData("Left Front: ", leftFrontSwitch.getState());
-        telemetry.addData("Right Front: ", rightFrontSwitch.getState());
-
+        telemetry.addData("Left Switch", bot.leftFrontSwitch.getState());
+        telemetry.addData("Right Switch", bot.rightFrontSwitch.getState());
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
+
     @Override
     public void stop() {
     }
