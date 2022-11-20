debbb31b564343d8c8f321a33c370e697a8b6287
Ezra <easpecht09@gmail.com>
2022-11-20 15:59:41 -0500

    autonomous test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EzraAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EzraAutonomous.java
@@ -5,25 +5,48 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.tel
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name="Ezra: Autonomous", group="Iterative Opmode")
 public class EzraAutonomous extends OpMode {
 
-    public static double SQUARE_SIZE = -0.5;
+    public DcMotorEx leftFrontMotor;
+    public DcMotorEx rightFrontMotor;
+    public DcMotorEx leftBackMotor;
+    public DcMotorEx rightBackMotor;
+
+    public DcMotorEx linearSlideMotor;
+    public Servo clawServo;
+
+    public static double BOT_SPEED = -0.5;
+    public static int squareSize = 24;
+    public static double circumference = 1.91;
+    public static double ticksPerRotation = 537.6;
 
     BigBob bot;
+    public void move(double squares, double drive, double rotation, double strafe, double scaleFactor) {
+        double leftFrontEncoder = leftFrontMotor.getCurrentPosition();;
+        double distance = squares * squareSize;
+        double rotations = distance / circumference;
+        double ticks = rotations * ticksPerRotation;
+        bot.moveBot(drive, rotation, strafe, scaleFactor);
+        while(leftFrontEncoder < ticks + leftFrontEncoder) {
+            // nothing
+        }
+        stop();
 
+    }
     public void init() {
 
         bot = new BigBob(hardwareMap);
     }
 
     public void start() {
-        bot.moveBot(SQUARE_SIZE, 0, 0, 0.5);
+        move(1, -0.05, 0, 0, 0.5);
     }
 
     @Override
     public void loop() {
-
     }
 }
