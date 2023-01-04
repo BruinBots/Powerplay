bc7bd17f277240b408df98f82948c58880614071
RockyShaikh <rockyjshaikh@gmail.com>
2023-01-03 20:03:52 -0500

    april tag beta

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EOCV/WebcamTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EOCV/WebcamTest.java
@@ -9,9 +9,6 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 @Autonomous(name = "Signal Sleeve Test")
 public class WebcamTest extends LinearOpMode {
 
