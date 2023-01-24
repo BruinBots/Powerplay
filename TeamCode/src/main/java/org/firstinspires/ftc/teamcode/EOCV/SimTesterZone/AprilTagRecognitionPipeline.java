33b6213b70cab061ae934102d0ac31e75be7e10c
Tanes <tkanchanawanchai.gmail.com>
2023-01-24 18:58:22 -0500

    Fix AprilTag merging error by removing HEAD and TAIL.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EOCV/SimTesterZone/AprilTagRecognitionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EOCV/SimTesterZone/AprilTagRecognitionPipeline.java
@@ -1,11 +1,6 @@
 package org.firstinspires.ftc.teamcode.EOCV.SimTesterZone;
 
-<<<<<<< HEAD
-//import org.firstinspires.ftc.teamcode.EOCV.SimTesterZone.SleeveDetection;
-=======
-//import org.firstinspires.ftc.teamcode.EOCV.SleeveDetection;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
->>>>>>> 3c112b1 (several april tags updates)
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -66,17 +61,12 @@ public class AprilTagRecognitionPipeline extends OpenCvPipeline {
     private float decimation;
     private boolean needToSetDecimation;
     private final Object decimationSync = new Object();
-
-<<<<<<< HEAD
     public AprilTagRecognitionPipeline(double tagsize, double fx, double fy, double cx, double cy) {
         this.tagsize = tagsize;
-=======
     public AprilTagRecognitionPipeline(Telemetry t)
     {
 
         this.telemetry = t;
-//        this.tagsize = tagsize;
->>>>>>> 3c112b1 (several april tags updates)
         this.tagsizeX = tagsize;
         this.tagsizeY = tagsize;
 //        this.fx = fx;
@@ -117,17 +107,11 @@ public class AprilTagRecognitionPipeline extends OpenCvPipeline {
         // Run AprilTag
         detections = AprilTagDetectorJNI.runAprilTagDetectorSimple(nativeApriltagPtr, grey, tagsize, fx, fy, cx, cy);
 
-<<<<<<< HEAD
         synchronized (detectionsUpdateSync) {
-=======
         telemetry.addData("Detections", detections.size() + "");
         if (detections.size() > 0) {
             telemetry.addData("ID", detections.get(0).id + "");
         }
-
-        synchronized (detectionsUpdateSync)
-        {
->>>>>>> 3c112b1 (several april tags updates)
             detectionsUpdate = detections;
         }
 
@@ -305,7 +289,6 @@ public class AprilTagRecognitionPipeline extends OpenCvPipeline {
         }
     }
 
-<<<<<<< HEAD
     public AprilTagRecognitionPipeline.ParkingPosition getParkingPosition() {
         ArrayList<AprilTagDetection> currentDetections = this.getLatestDetections();
         ParkingPosition position = ParkingPosition.CENTER;
@@ -325,21 +308,5 @@ public class AprilTagRecognitionPipeline extends OpenCvPipeline {
         }
 
         return position;
-=======
-    public AprilTagRecognitionPipeline.ParkingPosition getPosition() {
-//        if (detections.size() == 0) {
-//            return AprilTagRecognitionPipeline.ParkingPosition.CENTER;
-//        }
-//        else if (detections.get(0).id == ID_1) {
-//            return AprilTagRecognitionPipeline.ParkingPosition.LEFT;
-//        }
-//        else if (detections.get(0).id == ID_2) {
-//            return AprilTagRecognitionPipeline.ParkingPosition.CENTER;
-//        }
-//        else {
-//            return AprilTagRecognitionPipeline.ParkingPosition.RIGHT;
-//        }
-        return AprilTagRecognitionPipeline.ParkingPosition.CENTER;
->>>>>>> 3c112b1 (several april tags updates)
     }
 }
