97ac7f81c59b254779fb09cf19786b9c12b9b5ed
Sandra Mattar <69043333+smattar2020@users.noreply.github.com>
2023-01-24 16:32:57 -0500

    modifications to EdgeFollow_Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EdgeFollow_Testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EdgeFollow_Testing.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 
 import android.graphics.Color;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
@@ -12,6 +14,8 @@ import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import org.firstinspires.ftc.teamcode.Karen;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
+@Config
+@Autonomous()
 public class EdgeFollow_Testing extends LinearOpMode {
 
     Karen bot;
@@ -23,9 +27,11 @@ public class EdgeFollow_Testing extends LinearOpMode {
 
         colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
 
+        waitForStart();
+
         double rotate = 0.0;
 
-        boolean found = false;
+//        boolean found = false;
 
 //        while (!found) {
 //            bot.moveBot(1, 0, 0, 0.2);
@@ -63,25 +69,28 @@ public class EdgeFollow_Testing extends LinearOpMode {
 //            }
 //        }
 
-        while (true) {
-//            bot.moveBot(1, rotate, 0, 0.2); // move straight and then rotate to adjust
+        while (opModeIsActive()) {
+            bot.moveBot(1, rotate, 0, 0.2); // move straight and then rotate to adjust
 
             NormalizedRGBA colorsRGBA = colorSensor.getNormalizedColors();
             float[] hsv = {0, 0, 0};
             Color.colorToHSV(colorsRGBA.toColor(), hsv);
 
-            telemetry.addData("HSV", hsv);
-            telemetry.addData("RGBA", colorsRGBA);
+            telemetry.addData("HSV", hsv[0] + ", " + hsv[1] + ", " + hsv[2]);
+            telemetry.addData("RGBA", colorsRGBA.red + ", " + colorsRGBA.green + ", " + colorsRGBA.blue + ", " + colorsRGBA.alpha);
             telemetry.addData("rotate", rotate);
+            telemetry.update();
 
-            if (hsv[2] < 125) { // threshold between tape and no tape for the HSV value
-                rotate = -0.1;
+            if (hsv[0] < 200 && hsv[0] > 120) { // threshold between tape and no tape for the RGBA value
+                rotate = -0.5;
             }
             else {
-                rotate = 0.1;
+                rotate = 0.5;
             }
-
-            wait(1);
         }
     }
 }
+
+/*
+how does telemetry handle arrays??
+ */
